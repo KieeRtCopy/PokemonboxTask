0ff182af453d97a57a0944e1f0b92a93814072d2
test <dmytrolozyak@gmail.com>
2025-03-26 13:54:42 +0100

    update script

--- script.sh
+++ script.sh
@@ -54,19 +54,80 @@ INTERFACE_MODULE_SET=$MODULE_NAME"Interface"
 
 # Create the build.gradle file
 cat <<EOF > $MODULE_PATH/build.gradle
-apply plugin: 'com.android.library'
-apply plugin: 'common-binary-plugin'
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
 
 android {
+    namespace = "com.example.feature.$MODULE_NAME"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
     buildFeatures {
-        dataBinding true
-        viewBinding true
+        dataBinding = true
+        viewBinding = true
     }
 }
 
 dependencies {
-    implementation project(path: ':common')
+    implementation(project(":common"))
+    implementation(Libs.androidxCore)
+    implementation(Libs.androidxAppCompat)
+    implementation(Libs.material)
+    implementation(Libs.constraintLayout)
 
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.testExtJunit)
+    androidTestImplementation(Libs.espressoCore)
+    testImplementation(Libs.coroutinesTest)
+
+    implementation(Libs.daggerHiltAndroid)
+    kapt(Libs.daggerHiltCompiler)
+
+    implementation(Libs.navigationFragment)
+    implementation(Libs.navigationUi)
+    implementation(Libs.navigationDynamicFeaturesFragment)
+    androidTestImplementation(Libs.navigationTesting)
+
+    implementation(Libs.kotlinxSerializationJson)
+
+    implementation(Libs.retrofit)
+    implementation(Libs.gson)
+    implementation(Libs.retrofitConverterGson)
+    implementation(Libs.okhttp)
+    implementation(Libs.retrofitConverterScalars)
+    implementation(Libs.loggingInterceptor)
+
+    implementation(Libs.coil)
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 EOF
@@ -76,16 +137,80 @@ EOF
 
 # Create the build.gradle file
 cat <<EOF > $MODULE_INTERFACE_PATH/build.gradle
-apply plugin: 'com.android.library'
-apply plugin: 'common-binary-plugin'
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
 
 android {
+    namespace = "com.example.feature.$MODULE_INTERFACEO"
+    compileSdk = 35
 
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
-    implementation project(path: ':common')
+    implementation(project(":common"))
+    implementation(Libs.androidxCore)
+    implementation(Libs.androidxAppCompat)
+    implementation(Libs.material)
+    implementation(Libs.constraintLayout)
 
+    testImplementation(Libs.junit)
+    androidTestImplementation(Libs.testExtJunit)
+    androidTestImplementation(Libs.espressoCore)
+    testImplementation(Libs.coroutinesTest)
+
+    implementation(Libs.daggerHiltAndroid)
+    kapt(Libs.daggerHiltCompiler)
+
+    implementation(Libs.navigationFragment)
+    implementation(Libs.navigationUi)
+    implementation(Libs.navigationDynamicFeaturesFragment)
+    androidTestImplementation(Libs.navigationTesting)
+
+    implementation(Libs.kotlinxSerializationJson)
+
+    implementation(Libs.retrofit)
+    implementation(Libs.gson)
+    implementation(Libs.retrofitConverterGson)
+    implementation(Libs.okhttp)
+    implementation(Libs.retrofitConverterScalars)
+    implementation(Libs.loggingInterceptor)
+
+    implementation(Libs.coil)
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 EOF
